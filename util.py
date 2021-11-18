07f9e417e9965aeb26206d912457d8c2d7cc2487
ElifKarakutukDinc <elifkaraktk@gmail.com>
2021-11-18 21:07:38 +0100

    adding content for EDA file, and adding sessions_final_df_cleaning def in util file

--- util.py
+++ util.py
@@ -5,6 +5,41 @@ import seaborn as sns
 import warnings 
 warnings.filterwarnings("ignore")
 
+def file_read():
+    """
+    This function allows us to read all the Excel and Csv files into data frames.
+    After the initial reading, we delete rows which all columns are null.
+    :return: Dataframes
+    """
+    sessions_final_df = pd.read_csv("case_may_2020_sessions_final.csv")
+    tv_data_df = pd.ExcelFile("case_may_2020_TV_data.xlsx")
+    audience_df = tv_data_df.parse("Audience", usecols=["tv_show", "reach"])
+    tv_planning_df = tv_data_df.parse("TV Planning")
+    tv_spots_df = tv_data_df.parse("TV spots")
+    tv_visits_match_df = tv_data_df.parse("TV - visits match")
+
+    sessions_final_df.dropna(axis=0, how="all", inplace=True)
+    audience_df.dropna(axis=0, how="all", inplace=True)
+    tv_planning_df.dropna(axis=0, how="all", inplace=True)
+    tv_spots_df.dropna(axis=0, how="all", inplace=True)
+    tv_visits_match_df.dropna(axis=0, how="all", inplace=True)
+
+    return sessions_final_df, audience_df, tv_planning_df, tv_spots_df, tv_visits_match_df
+
+def sessions_final_df_cleaning(sessions_final_df,channel_list):
+    """
+    This function gathers a list from the user to parse the data frame to include the given channels.
+    :param channel_list: Channel list object to be parsed
+    :return: Dataframe
+    """
+    sessions_final_df_correct_channels = sessions_final_df[
+        sessions_final_df["channel"].isin(channel_list)
+    ].copy()
+
+    del sessions_final_df_correct_channels["Unnamed: 0"]
+
+    return sessions_final_df_correct_channels
+
 
 def df_first_look(df):
     """
