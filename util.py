1742f57c0dd8acdff36a87a911b93cd4f7d8b440
ElifKarakutukDinc <elifkaraktk@gmail.com>
2021-11-19 12:48:36 +0100

    adding countplot_viz def

--- util.py
+++ util.py
@@ -45,7 +45,6 @@ def df_first_look(df):
     """
     This function gets a Python Pandas dataframe and visualize basic information about the dataframe.
     :param df: Dataframe to be analyze
-    :return: This function doesn't return anything.
     """
     try:
         print("First 5 rows of dataframe:\n--------------------------\n", df.head())
@@ -72,3 +71,55 @@ def df_first_look(df):
 
     except Exception as e:
         print("Error at df_first_look function: ", str(e))
+        
+        
+def countplot_viz(
+    data,
+    xcolumn,
+    xlabel,
+    ylabel,
+    title,
+    hue=None,
+    fontsize_label=16,
+    fontsize_title=20,
+    fontsize_text=12,
+    rotation=45,
+    figsize_x=12,
+    figsize_y=5,
+    palette="viridis",
+):
+    """
+    This function gets a Python Pandas dataframe and visualize a countplot.
+    :param data: Dataframe to be analyze
+    :param xcolumn: This column designates x axis column.
+    :param xlabel: It designates name of x axis column.
+    :param ylabel: It designates name of y axis column.
+    :param title: This column designates name of graph.
+    :param hue: Name of variables in `data` or vector data, optional Inputs for plotting long-form data.
+    :param fontsize_label: It designates label size.
+    :param fontsize_title: It designates title size.
+    :param rotation: It designates rotation of graph.
+    :param palette: It designates colors of graph.
+    :return: This function doesn't return anything.
+    """
+    plt.figure(figsize=(figsize_x,figsize_y))
+    
+    g = sns.countplot(x=xcolumn, data=data, hue=hue, palette=palette)
+    g.set_title(title, fontsize=19)
+    g.set_xlabel(xlabel, fontsize=17)
+    g.set_ylabel(ylabel, fontsize=17)
+    g.set_xticklabels(g.get_xticklabels(), rotation=40, ha="right")
+    plt.tight_layout()
+    for p in g.patches:
+        height = p.get_height()
+        g.text(
+            p.get_x() + p.get_width() / 2.0,
+            height + 3,
+            "{:1}".format(height),
+            ha="center",
+            fontsize=fontsize_text,
+        )    
+    if hue != None:
+        g.legend(bbox_to_anchor=(1, 1), loc=1, borderaxespad=0)  
+
+        
