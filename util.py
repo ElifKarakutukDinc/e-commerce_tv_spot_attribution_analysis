0db564a4e378688fd2b71cff516ff7e3ac2335e4
ElifKarakutukDinc <elifkaraktk@gmail.com>
2021-11-19 16:15:02 +0100

    EDA on data sets

--- util.py
+++ util.py
@@ -104,7 +104,7 @@ def countplot_viz(
     """
     plt.figure(figsize=(figsize_x,figsize_y))
     
-    g = sns.countplot(x=xcolumn, data=data, hue=hue, palette=palette)
+    g = sns.countplot(x=xcolumn, data=data, hue=hue, palette=palette, order = data[xcolumn].value_counts().index)
     g.set_title(title, fontsize=19)
     g.set_xlabel(xlabel, fontsize=17)
     g.set_ylabel(ylabel, fontsize=17)
